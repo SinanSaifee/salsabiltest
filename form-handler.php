d46d94abb2c9749946730f5a8227908afbfa3286
prothasinan <149073608+prothasinan@users.noreply.github.com>
2023-12-30 17:30:11 +0600

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -1,32 +1,18 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
+    // Send email (modify this according to your needs)
+    $to = "amilulu.banu@example.com";
+    $headers = "From: $email";
 
-$email_from = 'info@salsabil.com';
- 
-$email_subject = 'New Form Submission ';
+    mail($to, $subject, $message, $headers);
 
-$email_body = "User Name:  $name.\n".
-               "User email:  $visitor_email.\n".
-                "Subject:  $subject.\n".
-                  "User message:  $message.\n".
-
-
-
-$to = 'salsabilcloth10@gmail.com';
-
-$headers = "From: $email_from\r\n";
-
-$headers .="Reply-To: $visitor_email\r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header('location: contact.html')
-
-
-
-
+    // Optionally, you can redirect the user after form submission
+    header("Location: contact.html");
+    exit();
+}
 ?>
